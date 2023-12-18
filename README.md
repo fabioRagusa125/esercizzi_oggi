35de1882493690f10e894372658bdb81596e60d8
fabio.r125 <ragusafabio3@gmail.com>
2023-12-18 10:01:36 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# esercizzi_oggi
---
+++ main.js
@@ -0,0 +1,157 @@
+
+
+
+// Esercizio n 1
+
+// /  let voto = 25
+
+//  console.log(voto)
+ 
+
+
+// if(voto < 18 && voto >= 0){
+//     console.log("bocciato");
+
+// } else if(voto >= 18 && voto <= 21){
+//     console.log("sufficente");
+
+// } else if(voto >=22 && voto <= 25){
+//     console.log("buono");
+
+// } else if(voto >=26 && voto <=29){
+//     console.log("distinto");
+
+// } else if(voto == 30){
+//     console.log("ottimo");
+
+// } else {
+//     console.log("voto )
+
+
+    // ESERCIZZIO N2
+
+    
+
+    //    let temperatura = Math.round(Math.random()* (40 - -15)- -15);
+
+
+
+    // if(temperatura < 20 && temperatura > 0){
+    //     console.log("non ci sono piu le mezze stagioni");
+
+        
+    // } 
+    //     else if(temperatura >= 30);{
+    //         console.log("lu mare ,lu sole,lu ventu");
+
+    //     }
+    //     else  if(temperatura <30 && temperatura >=20);{
+    //         console.log("mi dia una peroni sudata");
+    //      }
+    //        else if(temperatura <0 && temperatura < 10 );{
+    //         console.log("non e tanto il freddo quanto l'umidità");
+    //      }
+    //       else   if(temperatura < -10);{
+    //             console.log("copriti...ancora ti raffreddi");
+
+    //         }
+
+
+                //   ESERZIOZZIO 2 VARIANTE
+
+                // switch(true){
+                //     case ( temperatura < 20 && temperatura > 0):
+
+                //     console.log("non ci sono piu le mezze stagioni")
+                    
+                //         break;
+                //         case (temperatura >= 30):
+                //             console.log("lu mare, lu sole,lu ientu")
+                //             break;
+
+                //             case (temperatura < 30 && temperatura >= 20):
+                //                 console.log("mi dia una peroni sudata")
+                //                 break;
+
+                //                 case (temperatura < 0 && temperatura > -10):
+                //                     console.log("non e tanto il freddo quanto l'umidità")
+                //                     break;
+
+                //                     case ( temperatura < -10):
+                //                         console.log("copritiiii");
+
+                //                         default:
+                //                             console.log("tempeeratura non valida")
+                //                             break;
+
+                    
+                    
+                                            
+
+                    
+                        
+                        
+                      
+                   
+                        
+                        
+
+                    
+                      
+                        
+
+                     
+                  
+
+
+
+                
+
+
+                // // ESERCIZIO 4
+            
+                //     let player1 = 0;
+                //     let player2 = 0;
+                    
+                    
+                
+
+                    
+
+                //     let Tiri = 2;
+                    
+
+                //     for(let i = 0; i < Tiri; i++){
+                //         player1 = player1 + Math.round(Math.random()* (6 - 1)+ 1);
+                //         player2 = player2 + Math.round(Math.random()* (6 - 1)+ 1);
+
+                //     } 
+                //         console.log(player1, player2);
+
+                //         if(player1 > player2){
+                //             console.log("ha vinto il giocaore uno con ${player1 punti");
+                            
+                //         } else if(player2 > player1){
+                //             console.log("ha vinto il giocatore due con ${player2 punti");
+
+                //         }
+
+
+                        
+                        
+
+                        
+
+                        
+                        
+                    
+
+                
+                
+
+
+                
+    
+
+    
+
